fe2aa4a7f5ea821d585ea861012519a537c65f0a
Jesua Luján <code.masterqro@gmail.com>
2022-11-02 19:32:12 -0600

    importar CRUDAuthros, llamamos a la funcipon getAuthors

---
+++ 05.CRUDAuthors/main.js
@@ -0,0 +1,8 @@
+//? VAMOS A EJECUTAR NUESTRA LÓGICA
+//? MAIN ES NUESTRO JS PRINCIPAL
+
+const goodReadsCrud = require('./CRUDAuthors')
+
+//* LISTAR A TODOS LOS AUTORES 
+
+goodReadsCrud.getAuthors()
