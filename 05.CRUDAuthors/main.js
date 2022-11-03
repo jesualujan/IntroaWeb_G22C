4fe75add19494b0c6e3d0b5f28292a34f621cc54
Jesua Luján <code.masterqro@gmail.com>
2022-11-02 19:44:49 -0600

    ejecutar a getAuthor

--- 05.CRUDAuthors/main.js
+++ 05.CRUDAuthors/main.js
@@ -3,6 +3,9 @@
 
 const goodReadsCrud = require('./CRUDAuthors')
 
-//* LISTAR A TODOS LOS AUTORES 
 
-goodReadsCrud.getAuthors()
+//* LISTAR A TODOS LOS AUTORES 
+ // goodReadsCrud.getAuthors() 
+
+//* LISTAR UN AUTOR POR SU ID 
+     goodReadsCrud.getAuthor(14886)
