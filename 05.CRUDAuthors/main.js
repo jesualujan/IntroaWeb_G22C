714943a1f4a1e31eb123f1520b3f4cc923fa6bde
Jesua Luján <code.masterqro@gmail.com>
2022-11-02 20:30:33 -0600

    ejecutamos a createAuthor

--- 05.CRUDAuthors/main.js
+++ 05.CRUDAuthors/main.js
@@ -8,4 +8,17 @@ const goodReadsCrud = require('./CRUDAuthors')
  // goodReadsCrud.getAuthors() 
 
 //* LISTAR UN AUTOR POR SU ID 
-     goodReadsCrud.getAuthor(14886)
+ //  goodReadsCrud.getAuthor(14917)
+
+
+//* CREAR UN AUTOR (MANDAR UN JSON)
+    const jsonSEND = {
+        name: "LEO",
+        last_name: "MESSI",
+        nacionalidad: "MX",
+        biography: "FUTBOLISTA PROFESIONAL",
+        gender: "M",
+        age: 35,
+        is_alive: true
+    }
+goodReadsCrud.createAuthor(jsonSEND)
