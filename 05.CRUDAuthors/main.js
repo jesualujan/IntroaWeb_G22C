2821ff7942e08b60a6bac142e28b2c89e5b07c80
Jesua Luján <code.masterqro@gmail.com>
2022-11-02 21:08:40 -0600

    ejecutamos patchAuthor

--- 05.CRUDAuthors/main.js
+++ 05.CRUDAuthors/main.js
@@ -5,20 +5,29 @@ const goodReadsCrud = require('./CRUDAuthors')
 
 
 //* LISTAR A TODOS LOS AUTORES 
- // goodReadsCrud.getAuthors() 
+//  goodReadsCrud.getAuthors() 
 
 //* LISTAR UN AUTOR POR SU ID 
- //  goodReadsCrud.getAuthor(14917)
+//   goodReadsCrud.getAuthor(14917)
 
 
 //* CREAR UN AUTOR (MANDAR UN JSON)
-    const jsonSEND = {
-        name: "LEO",
-        last_name: "MESSI",
-        nacionalidad: "MX",
-        biography: "FUTBOLISTA PROFESIONAL",
-        gender: "M",
-        age: 35,
-        is_alive: true
+//     const jsonSEND = {
+//         name: "LEO",
+//         last_name: "MESSI",
+//         nacionalidad: "MX",
+//         biography: "FUTBOLISTA PROFESIONAL",
+//         gender: "M",
+//         age: 35,
+//         is_alive: true
+//     }
+// goodReadsCrud.createAuthor(jsonSEND)
+
+//* PATCH A UN AUTOR 
+    const jsonUpdate = {
+        name: "Itzel",
+        last_name: "Rocha",
+        biography: "CHEF EN PROCESO",
+        gender: "F"
     }
-goodReadsCrud.createAuthor(jsonSEND)
+goodReadsCrud.patchAuthor(14925, jsonUpdate)
